8cfdc71652801839f914139bb81745895db8e237
3thancs <ethancsloh@gmail.com>
2026-05-05 15:39:18 +1000

    Check and modify data entry to ensure accuracy

--- db_structure.sql
+++ db_structure.sql
@@ -240,18 +240,24 @@ INSERT INTO `competition_range_ends` (`competitionRecordedRangeID`, `endID`, `en
 (1, 1, 1, 1),
 (1, 2, 2, 1),
 (1, 3, 3, 1),
-(1, 4, 4, 1),
-(1, 5, 5, 1),
-(1, 6, 6, 1),
-(2, 7, 1, 2),
-(2, 8, 2, 2),
-(2, 9, 3, 2),
 (2, 10, 4, 2),
 (2, 11, 5, 2),
 (2, 12, 6, 2),
 (3, 13, 1, 3),
 (3, 14, 2, 3),
-(3, 15, 3, 3);
+(3, 15, 3, 3),
+(4, 1, 1, 4),
+(4, 2, 2, 4),
+(4, 3, 3, 4),
+(5, 4, 1, 5),
+(5, 5, 2, 5),
+(5, 6, 3, 5),
+(6, 7, 1, 6),
+(6, 8, 2, 6),
+(6, 9, 3, 6),
+(7, 10, 1, 7),
+(7, 11, 2, 7),
+(7, 12, 3, 7);
 
 -- --------------------------------------------------------
 
@@ -349,6 +355,28 @@ CREATE TABLE `individual_round_ranges` (
   PRIMARY KEY (`individualRecordedRangeID`),
   KEY `recordedRoundID` (`recordedRoundID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `individual_round_ranges`
+--
+
+TRUNCATE TABLE `individual_round_ranges`;
+INSERT INTO `individual_round_ranges` (`recordedRoundID`, `rangePositionNumber`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1),
+(10, 1),
+(11, 1),
+(12, 1),
+(13, 1),
+(14, 1),
+(15, 1);
 
 -- --------------------------------------------------------
 
@@ -487,7 +515,10 @@ INSERT INTO `round_ranges` (`roundID`, `rangeID`, `rangeSequence`) VALUES
 (11, 5, 3),
 (12, 7, 1),
 (12, 6, 2),
-(12, 5, 3);
+(12, 5, 3),
+(13, 7, 1),
+(13, 6, 2),
+(13, 5, 3);
 
 --
 -- Constraints for dumped tables
