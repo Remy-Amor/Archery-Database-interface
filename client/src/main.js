47dbae763ea2c8ea18d9710d8c538632e83178a8
Remy-Amor <remy.o.amor@gmail.com>
2026-05-19 16:18:48 +1000

    Imported bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import router from './router/index.js'
 import Paginate from 'vuejs-paginate-next'
 
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 createApp(App).
 use(router).
 component('Paginate', Paginate).
